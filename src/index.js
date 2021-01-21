36d24ed1b7f1e02df8d1809156139b48b6bb07cb
Techhadera <rdahad>
2021-01-21 15:28:15 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str == undefined || bracketsConfig == undefined) {
+    return false;
+  }
+
+  let configLeft = true;
+  while (str.length != 0 && configLeft) {
+    configLeft = false;
+
+    for (let config of bracketsConfig) {
+      let bracket = config.join('');
+      if (str.includes(bracket)) {
+        str = str.replace(bracket, '');
+        configLeft = true;
+      }
+    }
+  }
+
+  return str.length == 0;
 }
